a60ba52e6c1d61c265526c5b4599d31c0443e5f1
Hemanth Kumar <hemanthkumar4858@gmail.com>
2026-03-27 11:15:04 +0530

    img-changed

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,27 +1,27 @@
-// import avengersPoster from "./assets/avengers.jpg";
-// import batmanPoster from "./assets/batman.jpg";
-// import spidermanPoster from "./assets/spiderman.jpg";
+import avengers from "../assests/avengers.jpg";
+import batman from "../assests/batman.jpg";
+import spiderman from "../assests/spiderman.jpg";
 
 export const movies = [
     {
         id: 1,
         name: "Avengers",
         price: 200,
-        image: "https://wallpapers.com/images/hd/2560x1440-marvel-avengers-split-screen-7grdzwyde21jmqkb.jpg",
+        image: avengers,
         time: "10:00 AM",
     },
     {
         id: 2,
         name: "Batman",
         price: 180,
-        image: "https://static1.colliderimages.com/wordpress/wp-content/uploads/sharedimages/2024/04/the-batman-poster.jpg",
+        image: batman,
         time: "1:00 PM",
     },
     {
         id: 3,
         name: "Spiderman",
         price: 150,
-        image: "https://www.sonypictures.co.uk/sites/unitedkingdom/files/2020-12/spider_man_homecoming_rating_0.jpg",
+        image: spiderman,
         time: "6:00 PM",
     },
 ];
