10a6a24a09353a9cfe99d47d14eea93649122f03
Hemanth Kumar <hemanthkumar4858@gmail.com>
2026-03-30 08:45:18 +0530

    updated movies & assests

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,6 +1,10 @@
 import avengers from "../assests/avengers.jpg";
 import batman from "../assests/batman.jpg";
 import spiderman from "../assests/spiderman.jpg";
+import devil from "../assests/devil.jpg";
+import toxic from "../assests/toxic.jpg";
+import mark from "../assests/mark.jpg";
+import dhurandhar from "../assests/dhurandhar-2.jpg";
 
 export const movies = [
     {
@@ -24,4 +28,32 @@ export const movies = [
         image: spiderman,
         time: "6:00 PM",
     },
+    {
+        id: 4,
+        name: "Devil",
+        price: 300,
+        image: devil,
+        time: "9:00 PM",
+    },
+    {
+        id: 5,
+        name: "Toxic",
+        price: 350,
+        image: toxic,
+        time: "10:00 AM",
+    },
+    {
+        id: 6,
+        name: "Mark",
+        price: 250,
+        image: mark,
+        time: "11:00 AM",
+    },
+    {
+        id: 7,
+        name: "Dhrundhar-2",
+        price: 100,
+        image: dhurandhar,
+        time: "12:00 PM",
+    }
 ];
